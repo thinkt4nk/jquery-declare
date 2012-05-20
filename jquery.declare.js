25b53a11fa3381c15f3f803ea6f1dfee09def29a
Ryan Bales <thinkt4nk@gmail.com>
2012-05-20 09:31:05 -0500

    initial load

---
+++ jquery.declare.js
@@ -0,0 +1,31 @@
+/**
+ * jQuery Declare Plugin
+ * Provides support for namespacing objects
+ *
+ * @author Ryan Bales
+ * @date 2012-05-20
+ */
+
+
+(function($) {
+	var 
+		namespace_separator = /\./,
+		root = this;
+
+	$.extend({
+
+		declare: function(name, obj) {
+			namespace_members = name.split(namespace_separator);
+			current_namespace = root;
+
+			$.each(namespace_members, function(i,member) {
+				current_namespace[member] = current_namespace[member] || {};
+				current_namespace = current_namespace[member];
+				if (i === (namespace_members.length - 1)) {
+					$.extend(current_namespace, obj);
+				}
+			});
+		}
+	});
+
+})(jQuery);
